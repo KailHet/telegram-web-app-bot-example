ca30f90bf56686447d94edc5af1581609b567d46
KailHet <71993307+KailHet@users.noreply.github.com>
2023-02-12 18:34:36 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,24 @@ function checkCounter(id) {
   }
 }
 
+function openBucket() {
+  let bucketItems = document.getElementById("bucketList")
+  bucketItems.innerHTML = `
+  <div class="bitem">
+    <img src="assets/images/1.png" alt="" class="bimg">
+    <h2 class="counter" id="counter5">0</h2>
+    <div class="item-name">item name</div>
+    <div class="item-price">499 ₽</div>
+    <button class="btn-rem" id="btnm5" onclick="btnRem(id.replace('btnm', '') - 1)">-</button>
+    <button class="btn-add" id="btnp5" onclick="btnAdd(id.replace('btnp', '') - 1)">+</button>
+  </div>
+  `
+  bucket.style.animation = "bucketShowAnimation .3s forwards"
+  bucket.style.display = "block";
+  let sum = document.querySelectorAll("#item-price")
+  tg.MainButton.setText(`Оплатить ${sum}`)
+}
+
 function btnRem(id) {
   items.splice(items.indexOf(id)-1, 1);
   
@@ -44,19 +62,40 @@ function btnAdd(id) {
   counters[id].style.display = "inline-block";
   tg.MainButton.show()
   count += 1;
+  console.log(items.length)
 
   if (items.includes(`${id} - `)) items.splice(items.indexOf(id)-1, 1)
-  items.push({
-    id: id,
-    name: `${tg.initDataUnsafe.first_name} ${tg.initDataUnsafe.last_name}`,
-    price: Number(tg.initDataUnsafe.price),
-    count: counters[id].textContent
-  })
+  for (let i = 0; i <= items.length; i++) {
+    console.log(items)
+    if (items.length !== 0 && items[i].id == id) {
+      // items[i]
+      items[i].count += 1;
+    } else if (i == items.length) {
+      items.push({
+        id: id,
+        name: `${tg.initDataUnsafe.first_name} ${tg.initDataUnsafe.last_name}`,
+        price: Number(tg.initDataUnsafe.price),
+        count: 1
+      })
+    }
+  }
+
+  // items.push({
+  //   id: id,
+  //   name: `${tg.initDataUnsafe.first_name} ${tg.initDataUnsafe.last_name}`,
+  //   price: Number(tg.initDataUnsafe.price),
+  //   count: counters[id].textContent
+  // })
+
 
 
   // if (counters[id].textContent == `1`) {
   //   let bucketItems = document.getElementById("bucketList")
-  //   bucketItems.innerText = items.join(`\n`)
+  //   let text = []
+  //   for (let i = 0; i < items.length; i++) {
+  //     text.push(`${items[i].name} - ${items[i].count}`)
+  //   }
+  //   bucketItems.innerText = text.join(`\n`)
   //   bucket.style.animation = "bucketShowAnimation .3s forwards"
   //   bucket.style.display = "block";
   // }
@@ -68,10 +107,7 @@ function editOrder() {
 }
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-  let bucketItems = document.getElementById("bucketList")
-  bucketItems.innerText = items.join(`\n`)
-  bucket.style.animation = "bucketShowAnimation .3s forwards"
-  bucket.style.display = "block";
+  openBucket()
   // tg.sendData(item)
 })
 
